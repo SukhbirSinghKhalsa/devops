f144efe8dafd9a628edae3b7ec6f4740e6810702
SukhbirSinghKhalsa <sukhbirkhalsait@gmail.com>
2025-05-18 14:43:24 +0530

    updated looping and provider version

--- 1_Terraform/looping.tf
+++ 1_Terraform/looping.tf
@@ -4,6 +4,14 @@
 # no flexibility of changing names
 # no flexibikity in deletion also, may need to delete all the resources
 
+#---------------------------start of Count loop
+#COUNT meta_argument and count.index object
+resource "azurerm_resource_group" "resource_group" {
+  count    = 4
+  name     = count.index #iterator
+  location = "central india"
+}
+
 
 #COUNT meta_argument demonstration with String Interpolation (concatination)
 resource "azurerm_resource_group" "resource_group" {
@@ -13,12 +21,56 @@ resource "azurerm_resource_group" "resource_group" {
 }
 
 
-#list converted to set to iterate it and used with for each to create 3 resource groups
+variable "list-of-rg" {
+  type    = list(string)
+  default = ["rg-prod-001", "rg-dev-001", "rg-sandbox-001"]
+}
+#COUNT meta_argument with variable of type list and accessing list element
+resource "azurerm_resource_group" "resource_group" {
+  count    = 3
+  name     = var.list-of-rg[0] #String Interpolation
+  location = "central india"
+}
+
+
+#COUNT meta_argument with variable of type list + length function for dynamic execution
+resource "azurerm_resource_group" "resource_group" {
+  count    = 3
+  name     = var.list-of-rg[count.index] #String Interpolation
+  location = "central india"
+}
+
+
+#COUNT meta_argument with variable of type list + length function for dynamic execution
+resource "azurerm_resource_group" "resource_group" {
+  count    = length(var.list-of-rg)
+  name     = var.list-of-rg[count.index] #String Interpolation
+  location = "central india"
+}
+#---------------------------start of Count loop
+
+
+
+#---------------------------start of for_each loop
+variable "list-of-rgs" {
+  default = ["rg-prod-001", "rg-dev-001", "rg-sandbox-001"]
+}
+
+#list to iterate for_each errored out -PROBLEM
+# resource "azurerm_resource_group" "resource_group_1" {
+#   for_each = var.list-of-rgs
+#   name     = each.value
+#   location = "west us"
+# }
+
+
+#- SOLUTION1 - list converted to set to iterate it and used with for each to create 3 resource groups 
 resource "azurerm_resource_group" "resource_group_1" {
   for_each = toset(["1", "2", "3"])
   name     = "rg-sandbox-00${each.value}"
   location = "west us"
 }
+
 
 #Basic Map with 2 key value pairs and used with for each t create 2 resouce groups
 resource "azurerm_resource_group" "resource_group_2" {
@@ -34,15 +86,15 @@ resource "azurerm_resource_group" "resource_group_2" {
 resource "azurerm_resource_group" "resource_group_1" {
   for_each = {
     rg1 = {
-      mehdi = "ssk"
+      resource_group_name = "ssk"
       loc   = "west us"
     },
     rg2 = {
-      mehdi = "mehdi-bhai"
+      resource_group_name = "mehdi-bhai"
       loc   = "central india"
     }
   }
-  name     = each.value.mehdi
+  name     = each.value.resource_group_name
   location = each.value.loc
 }
 
